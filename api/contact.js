a6f9dd0434850f669477c3da4378c6fc33950dd1
zsimens <153461650+zsimens@users.noreply.github.com>
2026-04-01 13:48:32 -0700

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -88,4 +88,4 @@ export default async function handler(req, res) {
     console.error('Apollo error:', err);
     return res.status(500).json({ error: 'Failed to save contact' });
   }
-}
+};
